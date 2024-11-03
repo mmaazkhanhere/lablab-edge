7bf6b04e3c922098924cb8a302c73db06331efeb
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2024-11-03 20:15:41 +0500

    fixing replicate issue

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,10 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 from models.emotion_therapy import emotion_therapy
-# from models.prompt_for_image_generation import prompt_for_images
-# from models.prompt_for_music_generation import prompt_for_music
-# from models.image_generation import image_generation
-# from models.music_generation import generate_music
+from models.prompt_for_image_generation import prompt_for_images
+from models.prompt_for_music_generation import prompt_for_music
+from models.image_generation import image_generation
+from models.music_generation import generate_music
 
 class UserMemory(BaseModel):
     memory: str
@@ -49,35 +49,35 @@ async def memory_emotional_therapy(request: Request):
     return StreamingResponse(emotion_therapy(memory), media_type="text/plain")
 
 
-# # image generation endpoint
-# @app.post('/image')
-# async def image_analyzer(input: UserMemory):
-#     try:
-#         prompt: str = prompt_for_images(input.memory)
-#         image_response = image_generation(prompt, num_images=4)
-#         if not image_response.get("image_urls"):
-#             raise HTTPException(status_code=500, detail="No images were generated.")
+# image generation endpoint
+@app.post('/image')
+async def image_analyzer(input: UserMemory):
+    try:
+        prompt: str = prompt_for_images(input.memory)
+        image_response = image_generation(prompt, num_images=4)
+        if not image_response.get("image_urls"):
+            raise HTTPException(status_code=500, detail="No images were generated.")
 
-#         return {"image_urls": image_response["image_urls"]}
+        return {"image_urls": image_response["image_urls"]}
 
-#     except HTTPException as http_exc:
-#         raise http_exc
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
 
-# # music generation endpoint
-# @app.post('/music')
-# async def music_generator(input: UserMemory):
-#     """
-#     Endpoint to generate music based on user-provided memory.
-#     """
+# music generation endpoint
+@app.post('/music')
+async def music_generator(input: UserMemory):
+    """
+    Endpoint to generate music based on user-provided memory.
+    """
 
-#     try:
-#         prompt = prompt_for_music(input.memory)
-#         audio_url = generate_music(prompt)
-#         return MusicResponse(audio_url=audio_url)
-#     except HTTPException as he:
-#         raise he
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail="An unexpected error occurred.")
+    try:
+        prompt = prompt_for_music(input.memory)
+        audio_url = generate_music(prompt)
+        return MusicResponse(audio_url=audio_url)
+    except HTTPException as he:
+        raise he
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
--- backend/models/prompt_for_image_generation.py
+++ backend/models/prompt_for_image_generation.py
@@ -1,14 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-from openai import OpenAI
-from openai.types.chat import ChatCompletion
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
 
-client: OpenAI = OpenAI(
-    api_key=os.getenv('API_KEY'),
-    base_url='https://api.aimlapi.com'
+base_url = 'https://api.rhymes.ai/v1'
+api_key = os.getenv("ARIA_API_KEY")
+
+client: ChatOpenAI = ChatOpenAI(
+    model="aria",
+    base_url=base_url,
+    api_key=api_key,
+    streaming=False,
+    max_tokens=512
 )
 
 def prompt_for_images(memory: str)-> str:
@@ -24,29 +30,13 @@ def prompt_for_images(memory: str)-> str:
                         user emotionally
 
     """
-    
-    response: ChatCompletion = client.chat.completions.create(
-    model ="meta-llama/Llama-3.2-3B-Instruct-Turbo",
-    messages=[
-            {
-                "role": "system", 
-                "content": f"""You are an AI designed to create prompts for an image generation model based 
-                on a user’s emotional memory {memory}. Your task is to ensure that the generated image 
+
+    response = client.invoke([
+        SystemMessage(content=f"""You are an AI designed to create prompts for an image generation model based on a user’s emotional memory {memory}. Your task is to ensure that the generated image 
                 accurately reflects the memory’s specific details and emotional atmosphere. Analyze the 
-                memory for key visual elements such as people, places, objects, and activities, along with 
-                the emotional tone, whether it be nostalgia, joy, peace, or any other feeling. Your prompt 
-                should clearly describe these elements, including settings, individuals involved, and any 
-                significant objects, while conveying the right mood through the description of lighting, 
-                colors, and overall atmosphere. The goal is to guide the model to create an image that not 
-                only mirrors the memory but also evokes the emotions tied to it, with sufficient detail to 
-                reflect both the context and the emotional depth of the user's recollection."""
-            },
-            {
-                "role": "user", 
-                "content": memory
-            }
-        ]   
-    )
+                memory for key visual elements such as people, places, objects, and activities, along with the emotional tone, whether it be nostalgia, joy, peace, or any other feeling. Your prompt should clearly describe these elements, including settings, individuals involved, and any significant objects, while conveying the right mood through the description of lighting, colors, and overall atmosphere. The goal is to guide the model to create an image that not only mirrors the memory but also evokes the emotions tied to it, with sufficient detail to reflect both the context and the emotional depth of the user's recollection."""),
+        HumanMessage(content=memory)
+    ])
     
-    prompt: str = response.choices[0].message.content
+    prompt: AIMessage = response.content
     return prompt
