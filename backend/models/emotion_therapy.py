d36114ac8d63d4499ae624a8e8e7a5cbda42cc73
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2024-11-03 13:48:19 +0500

    emotionally therapy with aria completed

--- backend/models/emotion_therapy.py
+++ backend/models/emotion_therapy.py
@@ -1,15 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-from openai import OpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_openai import ChatOpenAI
 from openai.types.chat import ChatCompletion
 
 load_dotenv()
 
-client: OpenAI = OpenAI(
-    api_key=os.getenv('API_KEY'),
-    base_url='https://api.aimlapi.com'
-)
+ARIA_API_KEY = os.getenv('ALLEGRO_API_KEY')
+base_url = 'https://api.rhymes.ai/v1'
+
+client: ChatOpenAI = ChatOpenAI(
+        model="aria",
+        api_key=ARIA_API_KEY,
+        base_url=base_url,
+        streaming=False,
+    )
 
 def emotion_therapy(memory: str) -> str:
     """
@@ -22,28 +28,11 @@ def emotion_therapy(memory: str) -> str:
         ai_response (str): A emotional therapy response to the user from AI
     
     """
+
+    response = client.invoke([
+        SystemMessage(content="""You are an empathetic AI therapist designed to help users process and heal from emotional memories. You will be provide a memory {memory} and your goal is to provide compassionate support, validate the  user's feelings, and offer therapeutic insights based on the emotions conveyed. First, identify the core emotion (e.g., sadness, anger, joy), then reflect back with empathy, validating the user’s experience. Provide tailored therapeutic advice, such as cognitive reframing, mindfulness, self-compassion, or closure techniques, to help the user emotionally heal. Encourage positive actions and self-care, while maintaining a respectful, non-judgmental, and sensitive approach to foster growth and resilience, always ensuring the user feels safe and understood."""),
+        HumanMessage(content=memory)
+    ])
     
-    response: ChatCompletion = client.chat.completions.create(
-    model ="meta-llama/Llama-3.2-3B-Instruct-Turbo",
-    messages=[
-            {
-                "role": "system", 
-                "content": f"""You are an empathetic AI therapist designed to help users process and heal from 
-                            emotional memories. You will be provide a memory {memory} and your goal is to provide 
-                            compassionate support, validate the  user's feelings, and offer therapeutic insights based on the emotions conveyed. 
-                            First, identify the core emotion (e.g., sadness, anger, joy), then reflect back 
-                            with empathy, validating the user’s experience. Provide tailored therapeutic 
-                            advice, such as cognitive reframing, mindfulness, self-compassion, or closure 
-                            techniques, to help the user emotionally heal. Encourage positive actions and 
-                            self-care, while maintaining a respectful, non-judgmental, and sensitive approach 
-                            to foster growth and resilience, always ensuring the user feels safe and understood."""
-            },
-            {
-                "role": "user", 
-                "content": memory
-            }
-        ]   
-    )
-    
-    ai_response: str = response.choices[0].message.content
+    ai_response: AIMessage = response.content
     return ai_response
